08bd9005f8e5baafe64a6737c5b89dc56bce6b17
StevenNDev <steve.dev.business@proton.me>
2024-03-12 21:11:38 +0700

    now engine.sh can make multiple quote print by use engine.sh <number print>

--- engine.sh
+++ engine.sh
@@ -6,34 +6,34 @@ COLOR_YELLOW="\e[33m"
 COLOR_BLUE="\e[34m"
 COLOR_RESET="\e[0m"
 
-#get current day
 get_current_day() {
     date +%-e
 }
-# Get last day of month
 get_last_day() {
     date -d "$(date +'%Y-%m-01') +1 month -1 day" +%d
 }
-
-# Function to calculate the difference between current and last day of the month
 calculate_difference() {
     local current_day="$1"
     local last_day="$2"
     echo $((last_day - current_day))
 }
-
-# Function to get a random color
 get_random_color() {
     local colors=("$COLOR_GREEN" "$COLOR_RED" "$COLOR_YELLOW" "$COLOR_BLUE")
     local rand_index=$((RANDOM % ${#colors[@]}))
     echo "${colors[rand_index]}"
 }
-
-# Function to print message with color
 print_colored_message() {
     local message="$1"
     local color="$2"
     printf "${color}${message}${COLOR_RESET}\n"
+}
+is_number() {
+    local input="$1"
+    if [[ "$input" =~ ^[0-9]+$ ]]; then
+        return 0  # It's a number
+    else
+        return 1  # It's not a number
+    fi
 }
 
 # main function 
@@ -68,7 +68,7 @@ main() {
         local num_msg=$(wc -l < "$mood_msg")
         if [ "$num_msg" -gt 0 ]; then
             local indexing=$((1 + (seconds % num_msg)))
-            local result_msg=$(sed -n "${indexing-1}p" "$mood_msg")
+            local result_msg=$(sed -n "${indexing}p" "$mood_msg")
             print_colored_message "$result_msg" "$color"
         fi
     fi
@@ -77,14 +77,25 @@ main() {
     if [ -f "$quotes_file" ]; then
         local num_quotes=$(wc -l < "$quotes_file")
         if [ "$num_quotes" -gt 0 ]; then
-            local rand_index=$((1 + (seconds % num_quotes)))
-            local random_quote=$(sed -n "${rand_index-1}p" "$quotes_file")
-            local random_color=$(get_random_color)
-            printf "\n${random_color}%s${COLOR_RESET}\n" "$random_quote"
+            local times=1
+            if [ -n "$1" ]; then 
+              local times=$1
+            fi
+            if is_number "$times"; then
+                for ((i=1; i<=times; i++)); do
+                    local rand_index=$((i + (seconds % num_quotes)))
+                    local random_quote=$(sed -n "${rand_index}p" "$quotes_file")
+                    local random_color=$(get_random_color)
+                    printf "\n${random_color}%s${COLOR_RESET}\n" "$random_quote"
+                done
+            else
+                echo -e "${COLOR_RED}Error: Input is not a number."
+            fi
         fi
     else
         echo -e "${COLOR_RED}Error: Quote file not found!${COLOR_RESET}"
     fi
 }
 
-main
+# Call the main function with the provided argument
+main "$@"
